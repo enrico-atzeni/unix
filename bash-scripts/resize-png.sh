6631ab122d635706828a241810893e064d86f920
Enrico Atzeni <enrico.atzeni@outlook.com>
2016-06-12 15:19:11 +0200

    Add files via upload

---
+++ bash-scripts/resize-png.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# come si usa?
+# 1: crea una cartella "resized"
+# 2: ricrea al suo interno la struttura delle sottocartelle presenti
+# 3: lancia questo script da terminale. 
+
+for f in `find . -name "*.png"`;
+	do convert $f -gravity center -resize 320x220^ -crop 320x220+0+0 resized/$f.png;
+done
